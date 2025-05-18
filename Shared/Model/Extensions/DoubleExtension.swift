bcaf2ad7306d3da68d0a7fd114e2e7980a96adab
NicParsons <96993843+NicParsons@users.noreply.github.com>
2025-05-18 19:17:41 +1000

    Add NowPlayingView and add it to RecordingView
    * add methods to Model for seekBackwards(), seekForwards(), and updatePlaybackPosition(to:)

---
+++ Shared/Model/Extensions/DoubleExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DoubleExtension.swift
+//  AudioDiary
+//
+//  Created by Nicholas Parsons on 18/5/2025.
+//
+
+import Foundation
---
+++ Shared/Views/Helper Views/NowPlayingView.swift
@@ -0,0 +1,18 @@
+//
+//  NowPlayingView.swift
+//  AudioDiary
+//
+//  Created by Nicholas Parsons on 18/5/2025.
+//
+
+import SwiftUI
+
+struct NowPlayingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NowPlayingView()
+}
